0b24b267109949e82b293b4a15c10fa5bfb473ac
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2025-01-07 13:45:54 -0800

    add: ToolTip with hover to show and adjustable arrow position

---
+++ components/ui/ToolTip.tsx
@@ -0,0 +1,32 @@
+import clsx from 'clsx';
+import { useState } from 'react';
+
+const ToolTip = ({ show = true }) => {
+    const [visible, setVisible] = useState(false);
+
+    const arrowPosition = {
+        bottom: "bottom-[-4px] left-1/2 transform -translate-x-1/2 absolute border-t-8 border-x-8 border-t-neutral-950  border-x-transparent"
+    }
+
+    return (
+        <div
+            className='relative inline-flex items-center justify-center w-auto'
+            onMouseEnter={() => show && setVisible(true)}
+            onMouseLeave={() => show && setVisible(false)}
+        >
+            {visible &&
+                <>
+                    <div className='bg-neutral-950 rounded-lg px-3 py-1 inline-block w-auto shadow-lg'>
+                        <span className='text-xs font-medium text-white'>Insufficient stock</span>
+                    </div>
+                    <div className={`bottom-[-2px] left-1/2 transform -translate-x-1/2 absolute border-t-8 border-x-8 border-t-neutral-950  border-x-transparent`} ></div>
+                </>
+            }
+
+        </div>
+
+
+    )
+}
+
+export default ToolTip

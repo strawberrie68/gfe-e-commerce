96b78cac277dd8b9d2476cc9fc6c26a878e34e8e
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2025-01-07 14:08:57 -0800

    add: AvailableSize component to hold SizeSelector and label

---
+++ components/products/AvailableSize.tsx
@@ -0,0 +1,49 @@
+import { useProductDetailsContext } from "./ProductDetailsContext"
+import { getUnavailableSizes } from "./utils"
+import { useMemo } from 'react';
+import { Size } from "./SizeSelector";
+
+import SizeSelector from "./SizeSelector";
+
+const AvailableSize = () => {
+    const { selectedColor, product, selectedSize, setSelectedSize } =
+        useProductDetailsContext();
+
+    if (!product) {
+        return null;
+    }
+
+    const unavailableSizes = useMemo(
+        () => getUnavailableSizes({
+            product,
+            color: selectedColor ? { value: selectedColor, label: selectedColor } : { value: '', label: '' }
+        }),
+        [product, selectedColor]
+    );
+
+    const { sizes } = product;
+
+    return (
+        <fieldset className="flex flex-col items-start gap-4">
+            <legend className="font-normal text-sm text-neutral-500">Available Sizes</legend>
+            <div className="flex gap-2 flex-wrap mt-4">
+                {sizes.map((size) => {
+                    const isUnavailable = unavailableSizes.includes(size);
+                    const isSelected = selectedSize === size;
+
+                    return (
+                        <SizeSelector
+                            key={size}
+                            size={size as Size}
+                            inStock={!isUnavailable}
+                            isSelected={isSelected}
+                            onClick={() => setSelectedSize(size)}
+                        />
+                    )
+                })}
+            </div>
+        </fieldset>
+    )
+}
+
+export default AvailableSize

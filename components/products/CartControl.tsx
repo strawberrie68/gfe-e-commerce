7ef59ba3299ae0bfea277aa7523a8be2528c334c
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2025-01-07 16:03:15 -0800

    fix: cartcontrol height

--- components/products/CartControl.tsx
+++ components/products/CartControl.tsx
@@ -23,15 +23,15 @@ const CartControl = () => {
     return (
         <fieldset className="flex flex-col items-start gap-4">
             <label className="font-normal text-sm text-neutral-500">Quantity</label>
-            <div className="w-[125px] flex justify-center items-center text-neutral-600 gap-3 bg-neutral-50 p-0.5 rounded-md border border-solid border-neutral-200">
+            <div className="w-[125px] h-9 flex justify-center items-center text-neutral-600 gap-3 bg-neutral-50 p-0.5 rounded-md border border-solid border-neutral-200">
                 <button
                     type="button"
                     onClick={decrementQuantity}
                     disabled={isDecrementDisabled}
-                    className="min-w-11 min-h-11 flex justify-center items-center disabled:cursor-not-allowed">
-                    <Minus size={20} color={isDecrementDisabled ? "#a3a3a3" : "#525252"} />
+                    className="basis-4 min-w-11 min-h-11 flex justify-center items-center disabled:cursor-not-allowed">
+                    <Minus size={12} color={isDecrementDisabled ? "#a3a3a3" : "#525252"} />
                 </button>
-                <span>{itemQuantity}</span>
+                <span className="flex-grow">{itemQuantity}</span>
                 <ToolTip show={isIncrementDisabled} />
                 <button
                     type="button"

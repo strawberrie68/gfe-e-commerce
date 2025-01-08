1a875e4588fd2f269be313c7f43d920404d9f6eb
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2025-01-07 18:57:10 -0800

    update: selected colorSwatch css for ring when selected

--- components/products/ColorSwatch.tsx
+++ components/products/ColorSwatch.tsx
@@ -1,14 +1,14 @@
-import clsx from "clsx"
-import { Check } from "lucide-react"
+import clsx from "clsx";
+import { Check } from "lucide-react";
 
 interface ColorSwatchProps {
-    inStock: boolean
-    selected: boolean
+    inStock: boolean;
+    selected: boolean;
     color: {
-        value: string
-        label: string
-    }
-    onClick?: () => void
+        value: string;
+        label: string;
+    };
+    onClick?: () => void;
 }
 
 const RotatedLine = () => (
@@ -17,13 +17,19 @@ const RotatedLine = () => (
     </div>
 );
 
-const ColorSwatch: React.FC<ColorSwatchProps> = ({ inStock, selected, color, onClick }) => {
-
+const ColorSwatch: React.FC<ColorSwatchProps> = ({
+    inStock,
+    selected,
+    color,
+    onClick,
+}) => {
     return (
         <button
             type="button"
             onClick={onClick}
-            className={clsx("min-w-11 min-h-11 relative flex justify-center items-center")}
+            className={clsx(
+                "min-w-11 min-h-11 relative flex justify-center items-center"
+            )}
         >
             {!inStock && <RotatedLine />}
             <div
@@ -33,13 +39,14 @@ const ColorSwatch: React.FC<ColorSwatchProps> = ({ inStock, selected, color, onC
                     color.value === "#fff" && "border border-gray-200",
                     "hover:border-2 hover:border-indigo-200",
                     "focus:border-6 focus:border-indigo-200",
-                    "active:border-2 active:border-white active:ring-1 active:ring-indigo-700 active:ring-offset-indigo-700"
+                    selected &&
+                    "border-2 border-white ring-1 ring-indigo-700 ring-offset-indigo-700"
                 )}
             >
                 {selected && inStock && <Check color="#ffffff" />}
             </div>
         </button>
-    )
-}
+    );
+};
 
-export default ColorSwatch
+export default ColorSwatch;

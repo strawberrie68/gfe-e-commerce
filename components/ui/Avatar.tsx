0f8f4d23026027440e8bc5ea3a2652612ea1c5a3
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2025-01-10 17:18:40 -0800

    add: Avatar component

---
+++ components/ui/Avatar.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image"
+import clsx from "clsx"
+import { getUserInitial } from "../review/utils"
+
+interface AvatarProps {
+    image?: string | null,
+    name: string
+}
+
+const Avatar: React.FC<AvatarProps> = ({ image, name }) => {
+
+
+    return (
+        <div className={clsx(
+            "w-12 h-12 border rounded-full overflow-hidden",
+            "flex justify-center"
+        )}>
+            {image ?
+                <Image src={image} alt={`${name} Avatar`} width={48} height={48} className="object-cover" />
+                : <div className="bg-neutral-200 w-full flex justify-center items-center">
+                    <span className="font-medium text-xl text-center text-neutral-600">{getUserInitial(name)}</span>
+                </div>}
+
+        </div>
+    )
+}
+
+export default Avatar

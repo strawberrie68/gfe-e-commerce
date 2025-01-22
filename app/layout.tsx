2bb55b240c67b08f3b319489ed9e5ef8e6d1d85a
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2025-01-22 15:29:34 -0800

    remove: global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`antialiased min-h-screen`}
+        className={`antialiased`}
       >
         {children}
       </body>
